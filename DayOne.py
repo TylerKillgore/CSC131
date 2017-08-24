9cbc34c6e0394048c5f5864dc2c1063edaf15b86
Tyler Killgore <t.killgore.tk@gmail.com>
2017-08-24 09:59:00 -0400

    Add files via upload

---
+++ DayOne.py
@@ -0,0 +1,43 @@
+# Day 1 Exercise
+Tyler Killgore
+print("--"*10)
+"""
+0. Set up your one drive on your lab machine, then create a folder called "Day1".
+Put this file in the Day1 folder.
+After the file is synchronized, switch to another machine and access this file again.
+After finishing all those steps, print "OneDrive's ready" below.
+"""
+
+print("--"*10)
+"""
+Use print to answer the questions below:
+1. Why are you taking this course?
+
+* if a line is too long, try to break it like below (1-4):
+print(“I am Yang, and my favorite drink”)
+print(“is sparkling water”)
+"""
+print("I want to learn how programming interacts with computers at the most basic level "
+      "ie. high level language is turned into system language.")
+
+print("--"*10)
+"""
+2. Have you had any prior programming experience? If so, please elaborate.
+"""
+print("Some exposure to python, C#, html, css and javascript learned through various 'udemy' courses "
+      "and self study.")
+print("--"*10)
+"""
+3. What kind of learner are you (e.g. do you prefer learning concepts then 
+practice, or leaning concept in the process of practice? Do you prefer solving 
+problems when all the need concepts have been taught, or doing your own research 
+while solving problems?)?
+"""
+print("Typically a mix of auditory learner as well as learning through practice. Prefer to see proof of concept "
+      "before attempting a solution however, learning is most productive when attempting problem sets with "
+      "little to no instruction. Creating the necessity for in-depth research and the forcing function of establishing"
+      "a base of knowledge before trying.")
+print("--"*10)
+"""
+4. What additional information would you like to let your instructor know?
+"""
