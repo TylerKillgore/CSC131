29da252cc820533e9e6eeda108aeedaec0cfdd1f
Tyler Killgore <t.killgore.tk@gmail.com>
2017-09-06 21:46:05 -0400

    Add files via upload

---
+++ project_one_asci_design.py
@@ -0,0 +1,22 @@
+#Project One
+#Tyler Killgore
+#Asci Design from https://github.com/loeschg/ascii-art/blob/master/starwars/c3po-were-doomed.txt
+
+print("""
+
+                                       /~\                           
+                                      |o o)      We're doomed!       
+                                      _\=/_                          
+                     ___         #   /  _  \   #                     
+                    /() \         \\\//|/.\|\\\//                      
+                  _|_____|_        \/  \_/  \/                       
+                 | | === | |          |\ /|                          
+                 |_|  O  |_|          \_ _/                          
+                  ||  O  ||           | | |                          
+                  ||__*__||           | | |                          
+                 |~ \___/ ~|          []|[]                          
+                 /=\ /=\ /=\          | | |                          
+ ________________[_]_[_]_[_]________ /_]_[_\_________________________
+
+""")
+
